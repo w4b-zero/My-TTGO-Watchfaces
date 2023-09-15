c1a0e3326eb00fde40a5e348af64aad398d29f28
zero™ <w4b.zero@googlemail.com>
2023-09-15 06:19:24 +0200

    Update update_watchface_theme_list.sh

--- update_watchface_theme_list.sh
+++ update_watchface_theme_list.sh
@@ -9,5 +9,5 @@ github_repos()
   curl -s -H "Accept: application/vnd.github.v3+json" https://api.github.com/search/repositories?q=topic:my-ttgo-watch-face | \
   jq '[.items[] | {name: .name, home_url: ("https://github.com/" + .full_name), url: ("https://raw.githubusercontent.com/" + .full_name + "/" + .default_branch), download: (.html_url + "/releases/latest/download/watchface.tar.gz")}]'
 }
-curl https://raw.githubusercontent.com/PGNetHun/PG-TTGO-Watchfaces/main/watchface_theme_list.json > PGNetHun.json
-github_repos | jq --tab -s '(.[0] + .[1] + .[2]) | sort_by(.name)' local_watchface_theme_list.json PGNetHun.json - > watchface_theme_list.json
+curl https://raw.githubusercontent.com/w4b-zero/PG-TTGO-Watchfaces/main/watchface_theme_list.json > w4b-zero.json
+github_repos | jq --tab -s '(.[0] + .[1] + .[2]) | sort_by(.name)' local_watchface_theme_list.json w4b-zero.json - > watchface_theme_list.json
